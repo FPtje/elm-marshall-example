441ea76f49014a5fb2836a6ee476a656fd315185
Falco Peijnenburg <falco@lumiguide.nl>
2016-12-11 16:15:10 +0100

    Some progress, now hitting a real marshalling problem

--- elm/src/Ports.elm
+++ elm/src/Ports.elm
@@ -2,6 +2,8 @@ port module Ports exposing (..)
 
 import Types as T
 
+-- Sends the person to ghcjs
 port requestPersonChange : T.Person -> Cmd msg
 
+-- Gets a new person back from ghcjs
 port changedPerson : (T.Person -> msg) -> Sub msg
--- ghcjs/src/Main.hs
+++ ghcjs/src/Main.hs
@@ -1,11 +1,35 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import qualified "common" ElmMarshall.Types as T
 import "ghcjs-ffiqq" GHCJS.Foreign.QQ
+import "ghcjs-base" GHCJS.Types ( JSVal, jsval )
+import "ghcjs-base" GHCJS.Marshal ( fromJSValUnchecked )
+import qualified "ghcjs-base" GHCJS.Foreign.Callback as F
 
 modifyPerson :: T.Person -> T.Person
 modifyPerson p = T.Person (T.age p + 1337) (Just "This name is set from ghcjs")
 
 
+requestPersonChange :: JSVal -> IO ()
+requestPersonChange jsPerson = do
+  undefined
+  -- sendSubscriptionObject "changedPerson" <TODO>
+
+assignPortListener :: String -> F.Callback a -> IO ()
+assignPortListener portName callback =
+    let callback1 = jsval callback
+    in [jsi_| app.ports[ `portName ].subscribe( `callback1 ); |]
+
+
+sendSubscriptionObject :: String -> JSVal -> IO ()
+sendSubscriptionObject portName value = [js_| app.ports[ `portName ].send( `value ); |]
+
 main :: IO ()
-main = putStrLn "yes"
+main = do
+    portPersonChange <-
+      F.asyncCallback1 requestPersonChange
+
+    assignPortListener "requestPersonChange" portPersonChange
