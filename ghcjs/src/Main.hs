5b56c534a9d0694c91fdc1e3c3564107660a58f8
Falco Peijnenburg <falco@lumiguide.nl>
2017-02-05 11:02:37 +0100

    Converted main of example to elm-marshall.
    It doesn't work yet. I still have to fix an undefined in the fromElm function
    in elm-marshall

--- ghcjs/src/Main.hs
+++ ghcjs/src/Main.hs
@@ -5,6 +5,7 @@
 module Main where
 
 import qualified "common" ElmMarshall.Types as T
+import qualified "elm-marshall" Elm.Marshall as Elm
 import           "ghcjs-ffiqq" GHCJS.Foreign.QQ
 import           "ghcjs-base" GHCJS.Types ( JSVal, jsval )
 import           "ghcjs-base" GHCJS.Marshal ( fromJSValUnchecked, toJSVal )
@@ -12,56 +13,21 @@ import qualified "ghcjs-base" GHCJS.Foreign.Callback as F
 import qualified "ghcjs-base" JavaScript.Object as Obj
 import           "ghcjs-base" JavaScript.Object.Internal (Object(..))
 
+-- | Simple function to modify the person
 modifyPerson :: T.Person -> T.Person
 modifyPerson p = T.Person (T.age p + 1337) (Just "This name is set from ghcjs")
 
+-- | This function is bound to the requestPersonChange port in Elm in the main
+-- function. It simply changes the person and sends it back to elm through the
+-- changedPerson subscription
+requestPersonChange :: Elm.ElmApp -> T.Person -> IO ()
+requestPersonChange app person = do
+    putStrLn $ "Received person: " ++ show person
+    Elm.sendSubscriptionObject app "changedPerson" $ modifyPerson person
 
-personFromElm :: JSVal -> IO T.Person
-personFromElm jsv = do
-  let o = Object jsv
-  rawAge <- Obj.getProp "age" o
-  rawName <- Obj.getProp "name" o
-
-  age <- fromJSValUnchecked rawAge
-  name <- fromJSValUnchecked rawName
-
-  return $ T.Person age name
-
-personToElm :: T.Person -> IO JSVal
-personToElm person = do
-  obj <- Obj.create
-
-  jsAge <- toJSVal $ T.age person
-  jsName <- toJSVal $ T.name person
-
-  Obj.setProp "age" jsAge obj
-  Obj.setProp "name" jsName obj
-
-  return $ jsval obj
-
-
-requestPersonChange :: JSVal -> IO ()
-requestPersonChange jsPerson = do
-  person <- personFromElm jsPerson
-
-  let modified = modifyPerson person
-  print person
-  print modified
-  objModified <- personToElm modified
-  sendSubscriptionObject "changedPerson" objModified
-
-assignPortListener :: String -> F.Callback a -> IO ()
-assignPortListener portName callback =
-    let callback1 = jsval callback
-    in [jsi_| app.ports[ `portName ].subscribe( `callback1 ); |]
-
-
-sendSubscriptionObject :: String -> JSVal -> IO ()
-sendSubscriptionObject portName value = [js_| app.ports[ `portName ].send( `value ); |]
-
+-- | Main function. 
+-- Just gets the Elm app from a global and binds a listener to the requestPersonChange port
 main :: IO ()
 main = do
-    portPersonChange <-
-      F.asyncCallback1 requestPersonChange
-
-    assignPortListener "requestPersonChange" portPersonChange
+    app <- Elm.fromGlobalApp "app"
+    Elm.assignPortListener app "requestPersonChange" (requestPersonChange app)
